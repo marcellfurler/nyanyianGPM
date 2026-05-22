e533b1b3dc9054746d29450d8daf6b3f57dbf9ea
Jimm Daisy <118043258+marcellfurler@users.noreply.github.com>
2026-05-23 03:47:05 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
+import react from '@vitejs/react-refresh'
+
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/nyanyianGPM/',
 })
