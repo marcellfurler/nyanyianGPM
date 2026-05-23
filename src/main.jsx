adaba937eab96b7ed4eee44070deef7e33e5b2ef
Jimm Daisy <marcell.jureinwi@ti.ukdw.ac.id>
2026-05-23 11:54:00 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
@@ -13,13 +13,13 @@ import "./engine/css/player.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HalamanTransisi />} />
         <Route path="/NyanyianGPM" element={<HalamanUtama />} />
         {/* <Route path="/NyanyianGPM/Lagu" element={<HalamanRincianLagu />} /> */}
         <Route  path="/NyanyianGPM/Lagu/:songId" element={<HalamanRincianLagu  />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
